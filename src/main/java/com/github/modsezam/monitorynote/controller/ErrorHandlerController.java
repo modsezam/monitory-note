c8e76be893aca3bd3864a20d7b180689a00a415d
SzymonKuhn <szymon.kuhn@gmail.com>
2019-10-29 18:41:49 +0100

    work in progress

--- src/main/java/com/github/modsezam/monitorynote/controller/ErrorHandlerController.java
+++ src/main/java/com/github/modsezam/monitorynote/controller/ErrorHandlerController.java
@@ -1,16 +1,21 @@
 package com.github.modsezam.monitorynote.controller;
 
+import com.github.modsezam.monitorynote.model.ErrorMessage;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-public class MyErrorController implements ErrorController {
+public class ErrorHandlerController implements ErrorController {
 
     @RequestMapping("/error")
     public String handleError(HttpServletRequest req, Model model) {
@@ -20,6 +25,12 @@ public class MyErrorController implements ErrorController {
         }
         //do something like logging
         return "error";
+    }
+
+    @ExceptionHandler(value = {EntityNotFoundException.class})
+    public ResponseEntity<ErrorMessage> handleEntityNotFoundException (EntityNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorMessage("Unable to find entity: " + exception.getMessage()));
+
     }
 
     @Override
